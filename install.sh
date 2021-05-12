ba1e93aa1803381e5c109c010ebcd5f7d35c0ffd
alagroy- <alagroy-@student.42.fr>
2021-05-12 13:03:35 +0000

    adding file architecture + container setup

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+SHELL="/bin/zsh"
+git clone https://github.com/alagroy-42/push_script.git /tmp/push >/dev/null 2>&1
+cd /tmp/push
+zsh install.sh
+git clone https://github.com/alagroy-42/elf-extractor.git /tmp/extract >/dev/null 2>&1
+cd /tmp/extract
+zsh install.sh
+source ~/.zshrc
+echo "All utillities installed"
+cd -2
+rm install.sh
+git add install.sh
